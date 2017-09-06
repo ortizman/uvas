ca4e15d78a378ef6a6792dff4d2019d2c9525271
Manuel Ortiz <ortizman@gmail.com>
2017-09-06 00:13:00 -0300

    first commit

---
+++ server/boot/load-uvas.js
@@ -0,0 +1,54 @@
+const Horseman = require('node-horseman')
+const cheerio = require('cheerio')
+const _ = require('lodash')
+const q = require('q')
+
+module.exports = function (app) {
+    var horseman = new Horseman();
+    horseman
+        .userAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36')
+        .open('http://www.bcra.gob.ar/PublicacionesEstadisticas/Principales_variables_datos.asp?descri=22&fecha=Fecha_Cvs&campo=Cvs')
+        .wait(3000)
+        .type('select[name="desde"]', '31/03/2016')
+        .type('select[name="hasta"]', '05/09/2017')
+        .wait(1000)
+        .click('.btn-primary')
+        .keyboardEvent('keypress', 16777221)
+        .waitForSelector('#tabla')
+        .wait(1000)
+        .html()
+        .then(html => processInfo(html))
+        .then(uvas => {
+            app.models.Uva.create(uvas, function (err, resp) {
+                if (err) console.log(err);
+                else console.log('Se insertaron las uvas!!');
+            });
+        })// prints out the number of results
+        .close()
+
+    horseman.on('resourceError', function (err) {
+        console.log(err)
+    });
+};
+
+var processInfo = (html) => {
+
+    var def = q.defer();
+
+    try {
+        var $ = cheerio.load(html)
+        var rows = $('#tabla tbody tr').next().text().trim()
+        var values = rows.split('\n').map(function (e) { return e.trim() })
+        values = _.compact(values);
+        var uvas = []
+        for (var index = 0; index < values.length; index = index + 2) {
+            uvas.push({ fecha: values[index], valor: +values[index + 1].replace(/\./g, '').replace(',', '.') });
+        }
+        console.log(_.head(uvas))
+        def.resolve(uvas)
+    } catch (error) {
+        def.reject(error)
+    }
+
+    return def.promise;
+}
